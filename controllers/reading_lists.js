49d1e82c7950fdfcc7f767d11ee7a1da0a5d9e44
akuivan <akuivan@users.noreply.github.com>
2024-11-15 13:45:05 +0200

    Exercise 13.22 done

--- controllers/reading_lists.js
+++ controllers/reading_lists.js
@@ -2,7 +2,10 @@ require('express-async-errors')
 
 const router = require('express').Router()
 
+const jwt = require('jsonwebtoken')
+
 const { ReadingList, User, Blog } = require('../models')
+const { SECRET } = require('../util/config')
 
 
 const errorHandler = (err, req, res, next) => {
@@ -10,6 +13,20 @@ const errorHandler = (err, req, res, next) => {
     const status = err.status || 500 // Default to 500 if no status is set
     const message = err.message || 'Internal Server Error'
     res.status(status).json({ error: message })
+}
+
+const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      try {
+        req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+      } catch{
+        return res.status(401).json({ error: 'token invalid' })
+      }
+    }  else {
+      return res.status(401).json({ error: 'token missing' })
+    }
+    next()
 }
 
 router.post('/', async (req, res) => {
@@ -33,6 +50,36 @@ router.post('/', async (req, res) => {
         }
     } else {
         const error = new Error(`User or blog not found, unable to add blog to an user's reading list`)
+        error.status = 404
+        throw error
+    }
+})
+
+router.put('/:id', tokenExtractor, async (req,res) => {
+    const readingListId = req.params.id
+    const { read }= req.body
+
+    // Validate the read field
+    if (typeof read !== 'boolean') {
+        const error = new Error('Invalid value for "read" field')
+        error.status = 400
+        throw error
+    }
+        
+    const readingListEntry = await ReadingList.findByPk(readingListId)
+    
+    if (readingListEntry) {
+        if (readingListEntry.userId !== req.decodedToken.id) {
+            const error = new Error('Unauthorized access to this reading list entry')
+            error.status = 403
+            throw error
+        } else {
+            readingListEntry.read = read
+            await readingListEntry.save()
+            res.json(readingListEntry)
+        }
+    } else {
+        const error = new Error(`Reading list not found`)
         error.status = 404
         throw error
     }
